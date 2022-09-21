6e0dddf464c3f8a0632bf23e7024ecb847e09759
Ernesto Cadena Muñoz <ernestocadena@usantotomas.edu.co>
2022-09-21 11:07:56 -0500

    con css

--- ejercicio2/settings.py
+++ ejercicio2/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'ejercicio2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+        BASE_DIR / 'static/css' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
