b1f895f44c676b30c129c940f5101bc3fa0d38a4
Ernesto Cadena Muñoz <ernestocadena@usantotomas.edu.co>
2022-09-24 10:24:58 -0500

    Con psycopg2 apuntando a postgresql y un modelo para la bd

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
+import email
+import uuid
+from email.policy import default
+from enum import unique
+from operator import truediv
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    document = models.CharField(max_length=200)
+    email = models.TextField(null=True, blank=True)
+
+def __str__(self):
+    return self.name
