67519dddeba9d885e2919689f86fcdd6b69b0b51
Ernesto Cadena Muñoz <ernestocadena@usantotomas.edu.co>
2022-09-25 13:44:55 -0500

    Con usuario administrador log in y log out

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,9 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.projects, name="projects"),
-    path('project/<str:pk>', views.project, name="project"),
+    path('project/', views.project, name="project"),
+    path('signup/', views.signup, name="signup"),
+    path('login/', views.loginUser, name="login"),
+    path('main/', views.mainPage, name="main"),
+    path('logout/', views.logoutUser, name="logout"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 from django.http import HttpResponse
 from . import models
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def projects(request):
     proyectos=models.Proyecto.objects.all()
     return render(request, 'projects.html', {'proyectos':proyectos})
 
-def project(request, pk):
+def project(request):
     return render(request, 'single-project.html')
 
+def signup(request):
+    return render(request, 'signup.html')
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return('projects')
+
+    if request.method == 'POST':
+        username =request.POST['username']
+        password =request.POST['password']
+
+        try:
+            user= User.objects.get(username=username)
+        except:
+            print('Username does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('projects')
+        else:
+            print('Username OR password is incorrect')
+    return render(request, 'login.html')
+
+def mainPage(request):
+    return render(request, 'main.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
